afa16b88d7e18a96de03457cb0c6032d5e8ce76d
Éric Massé <Ericmas001@gmail.com>
2018-04-22 17:25:31 -0400

    0.1.2 Trace logger

---
+++ Com.Ericmas001.Logs/Services/TraceLoggerService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using Com.Ericmas001.Logs.Enums;
+using Com.Ericmas001.Logs.Services.Interfaces;
+
+namespace Com.Ericmas001.Logs.Services
+{
+    public class TraceLoggerService : ILoggerService
+    {
+        private readonly ILoggerConfiguration m_LoggerConfiguration;
+
+        public TraceLoggerService(ILoggerConfiguration loggerConfiguration)
+        {
+            m_LoggerConfiguration = loggerConfiguration;
+        }
+
+        public void Log(LogLevelEnum level, string message)
+        {
+            string TimeStamp() => $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] ";
+
+            if (level <= m_LoggerConfiguration.MinimumLogLevel)
+            {
+                Trace.WriteLine($"{(m_LoggerConfiguration.ShowTimestamp ? TimeStamp() : string.Empty)}{message}");
+            }
+
+        }
+    }
+}
---
+++ Com.Ericmas001.Logs/TraceLogsRegistrant.cs
@@ -0,0 +1,16 @@
+﻿using Com.Ericmas001.DependencyInjection.Registrants;
+using Com.Ericmas001.Logs.Enums;
+using Com.Ericmas001.Logs.Services;
+using Com.Ericmas001.Logs.Services.Interfaces;
+
+namespace Com.Ericmas001.Logs
+{
+    public class TraceLogsRegistrant : AbstractRegistrant
+    {
+        protected override void RegisterEverything()
+        {
+            RegisterInstance<ILoggerConfiguration>(new LoggerConfiguration{MinimumLogLevel = LogLevelEnum.Verbose});
+            Register<ILoggerService, ConsoleLoggerService>();
+        }
+    }
+}
